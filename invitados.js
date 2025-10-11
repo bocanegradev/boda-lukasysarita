1807f02caa4a8b2b63b93d31105f590a821fd238
Lukas <bocanegra.lukas@gmail.com>
2025-10-10 22:44:22 -0500

    Se agrega archivo .js con el listado de invitados y sus códigos

---
+++ invitados.js
@@ -0,0 +1,164 @@
+const listaInvitados = {
+    'pLw': {nombre: 'Rudy y Rocío Garrido', puestos: 2}
+    ,'qgk': {nombre: 'Ana María Garrido', puestos: 1}
+    ,'IbI': {nombre: 'Abuelito', puestos: 1}
+    ,'LBK': {nombre: 'Jose y Sara Mejía', puestos: 2}
+    ,'IdK': {nombre: 'Augusto Barragán', puestos: 1}
+    ,'6lD': {nombre: 'Andrés Bernal', puestos: 1}
+    ,'Put': {nombre: 'Daniel Suárez', puestos: 1}
+    ,'gVO': {nombre: 'Rubén Obando', puestos: 1}
+    ,'SNG': {nombre: 'Juanda Jiménez', puestos: 1}
+    ,'sJr': {nombre: 'Andrés Báez', puestos: 1}
+    ,'A8T': {nombre: 'Stepha', puestos: 1}
+    ,'nPh': {nombre: 'Diego Vásquez', puestos: 1}
+    ,'Dhj': {nombre: 'David Bravo', puestos: 1}
+    ,'uYg': {nombre: 'Lucho Andrade', puestos: 1}
+    ,'ydM': {nombre: 'Nath - Data', puestos: 1}
+    ,'Drn': {nombre: 'Jhon Panche', puestos: 1}
+    ,'Imi': {nombre: 'Edward Rojas y Señora', puestos: 2}
+    ,'g3W': {nombre: 'Pipe y Vale Hernández', puestos: 2}
+    ,'lkF': {nombre: 'David Hernández', puestos: 1}
+    ,'uRp': {nombre: 'Daniel y Jenny Beltrán', puestos: 2}
+    ,'9bk': {nombre: 'David Arana', puestos: 1}
+    ,'3lW': {nombre: 'Andrés Mosquera', puestos: 1}
+    ,'Q0V': {nombre: 'Linda Hernández', puestos: 1}
+    ,'5Ar': {nombre: 'Edgar y Liliana Toquica', puestos: 2}
+    ,'21g': {nombre: 'David Toquica', puestos: 1}
+    ,'Z6D': {nombre: 'Julián y Edna Palma', puestos: 2}
+    ,'OnZ': {nombre: 'Estelita Sánchez', puestos: 1}
+    ,'Bmy': {nombre: 'Magda Sánchez', puestos: 1}
+    ,'yi1': {nombre: 'Juanca y Laura Reina', puestos: 2}
+    ,'bf8': {nombre: 'Ricardo y Marta Mejía', puestos: 2}
+    ,'rci': {nombre: 'Mario Mejía', puestos: 1}
+    ,'dGO': {nombre: 'Carlos y Valeria Mejía', puestos: 2}
+    ,'6T6': {nombre: 'Memo y Erika Hernández', puestos: 2}
+    ,'6c6': {nombre: 'Nicolás y Laura Reyes', puestos: 2}
+    ,'BcZ': {nombre: 'Samuel Yánez y Cata Rubiano', puestos: 2}
+    ,'BV2': {nombre: 'Sebas Rubiano y Vale Castillo', puestos: 2}
+    ,'8YF': {nombre: 'Freddy y Paty Rodríguez', puestos: 2}
+    ,'3Rz': {nombre: 'Edilberto, Martica y Juli Bocanegra', puestos: 3}
+    ,'bc0': {nombre: 'Jairito, Anita y Josué Bocanegra', puestos: 3}
+    ,'UkI': {nombre: 'Ramoncito y Martica Riascos', puestos: 2}
+    ,'VK8': {nombre: 'Juan Andrés Riascos y Laurita', puestos: 2}
+    ,'QEq': {nombre: 'Camilo Riascos', puestos: 1}
+    ,'0Rw': {nombre: 'Jeffer, Anita y Macarena Riascos', puestos: 2}
+    ,'AkL': {nombre: 'Abuelita Clarita', puestos: 1}
+    ,'sic': {nombre: 'Andrés Peña', puestos: 1}
+    ,'Imn': {nombre: 'Héctor Peña y Samuel Peña', puestos: 2}
+    ,'VCQ': {nombre: 'Bladimir, Zuleny, Emanuel y María Peña', puestos: 4}
+    ,'7TF': {nombre: 'Juan Manuel y Aireen Galindo', puestos: 2}
+    ,'me0': {nombre: 'María Paula Ariza', puestos: 1}
+    ,'fdB': {nombre: 'Gabo y Ángela Serrano', puestos: 2}
+    ,'0br': {nombre: 'Pablo Vásquez', puestos: 1}
+    ,'cVY': {nombre: 'Angie Vásquez', puestos: 1}
+    ,'gx7': {nombre: 'Juan Tunja', puestos: 1}
+    ,'uxU': {nombre: 'Anita Tunja', puestos: 1}
+    ,'1vw': {nombre: 'Diego Alejandro Tunja', puestos: 1}
+    ,'tlx': {nombre: 'Diego Isaac Tunja', puestos: 1}
+    ,'Mcf': {nombre: 'Samuel Tunja', puestos: 1}
+    ,'muE': {nombre: 'Ricardo, Nati y Juampa Trujillo', puestos: 3}
+    ,'NmZ': {nombre: 'Carlitos, Luz Marina y Daniel Díaz', puestos: 3}
+    ,'PGQ': {nombre: 'Christian y Natalia Yanez', puestos: 2}
+    ,'YSZ': {nombre: 'Daniel Camacho', puestos: 1}
+    ,'fQO': {nombre: 'Juancho Camacho', puestos: 1}
+    ,'dO5': {nombre: 'Fidel y Linda Torres', puestos: 2}
+    ,'3vu': {nombre: 'Freddy y Martica Beltrán', puestos: 2}
+    ,'Q0U': {nombre: 'Helen Orozco', puestos: 1}
+    ,'LQk': {nombre: 'Jors y Joha Moreno', puestos: 2}
+    ,'CBt': {nombre: 'Juampa Mendoza y Eri Morales', puestos: 2}
+    ,'jfs': {nombre: 'Merly Gómez', puestos: 1}
+    ,'Iyj': {nombre: 'Oscar y Eli Salas', puestos: 2}
+    ,'dIT': {nombre: 'Pablo y Nadia Marín', puestos: 2}
+    ,'Mqt': {nombre: 'Pipe y Janis Fernández', puestos: 2}
+    ,'PjR': {nombre: 'Pipe Santamaría y Camila Galindo', puestos: 2}
+    ,'7ON': {nombre: 'Ricardo y Olgui Walteros', puestos: 2}
+    ,'aLE': {nombre: 'Sofía Walteros', puestos: 1}
+    ,'eJv': {nombre: 'Natalia Walteros', puestos: 1}
+    ,'I1J': {nombre: 'Samuel Sur y Cindy Sur', puestos: 2}
+    ,'znF': {nombre: 'Santi Villamil', puestos: 1}
+    ,'BSL': {nombre: 'Sebas y Nata Perilla', puestos: 2}
+    ,'vGm': {nombre: 'Sergio Hidalgo', puestos: 1}
+    ,'VgW': {nombre: 'Steven Ramírez y Daniela Higuera', puestos: 2}
+    ,'Jbu': {nombre: 'Wilson y Liliana Casas', puestos: 2}
+    ,'GU4': {nombre: 'Edwin Contreras', puestos: 1}
+    ,'MNX': {nombre: 'Wilson & Stellita Ruiz', puestos: 2}
+    ,'Jvb': {nombre: 'Camilo Ruiz', puestos: 1}
+    ,'56i': {nombre: 'Camila Ruiz', puestos: 1}
+    ,'ZKn': {nombre: 'David & Yudy Pantano', puestos: 2}
+    ,'xi2': {nombre: 'David & Paola Arias', puestos: 2}
+    ,'eOo': {nombre: 'Miguel & Daly Rodriguez', puestos: 2}
+    ,'hNX': {nombre: 'Alex & Tatiana Pinzón', puestos: 2}
+    ,'F19': {nombre: 'Sofia Romero', puestos: 1}
+    ,'0wQ': {nombre: 'Amparito Romero', puestos: 1}
+    ,'Dyq': {nombre: 'Paula Romero', puestos: 1}
+    ,'H8x': {nombre: 'Familia Niño Rueda', puestos: 4}
+    ,'2QE': {nombre: 'Paula Niño', puestos: 1}
+    ,'mzC': {nombre: 'Hugo & Cata Arias', puestos: 2}
+    ,'dCJ': {nombre: 'Reinaldo & Jenifer Blanco', puestos: 2}
+    ,'6YQ': {nombre: 'Isai Victorino', puestos: 1}
+    ,'d1C': {nombre: 'Humberto & Luna Victorino', puestos: 2}
+    ,'4ZF': {nombre: 'Raquel Victorino', puestos: 1}
+    ,'t9u': {nombre: 'Elisa Mona', puestos: 1}
+    ,'b5P': {nombre: 'Sebastián Quitián', puestos: 1}
+    ,'mkq': {nombre: 'Javier & Gloria', puestos: 2}
+    ,'BiP': {nombre: 'Camilo & Lorena', puestos: 2}
+    ,'yaC': {nombre: 'Paola Mendez', puestos: 1}
+    ,'6tI': {nombre: 'Brayhan Moreno', puestos: 1}
+    ,'poE': {nombre: 'Valentina Buitrago', puestos: 1}
+    ,'k10': {nombre: 'Diana Alvarez', puestos: 1}
+    ,'ves': {nombre: 'Oscar & Johana Bernal', puestos: 2}
+    ,'2Ti': {nombre: 'Carla Romero', puestos: 1}
+    ,'jC1': {nombre: 'Camilo Romero', puestos: 1}
+    ,'mSt': {nombre: 'Marlon Paez', puestos: 1}
+    ,'vmA': {nombre: 'Santiago Guavita', puestos: 1}
+    ,'eEi': {nombre: 'Juan Pablo & Milena Valiente', puestos: 2}
+    ,'w8l': {nombre: 'Vanessa', puestos: 1}
+    ,'rgb': {nombre: 'Valentina Rodríguez', puestos: 1}
+    ,'hp5': {nombre: 'Gerardo & Monica Corredor', puestos: 2}
+    ,'yoM': {nombre: 'Angell Ariza', puestos: 1}
+    ,'54f': {nombre: 'Valentina Ariza', puestos: 1}
+    ,'axe': {nombre: 'Saúl & Claudia Ariza', puestos: 2}
+    ,'vFe': {nombre: 'Alejandra Barragán', puestos: 1}
+    ,'LmR': {nombre: 'Alejandro Gómez', puestos: 1}
+    ,'OpK': {nombre: 'Tilcia Traslaviña', puestos: 1}
+    ,'xnl': {nombre: 'Neil Ariza', puestos: 1}
+    ,'zYf': {nombre: 'Maicol Ariza', puestos: 1}
+    ,'DbD': {nombre: 'Johana Niño', puestos: 1}
+    ,'7eN': {nombre: 'Lucero Ariza', puestos: 1}
+    ,'zHp': {nombre: 'Camilo & Sofia Ariza', puestos: 2}
+    ,'ttL': {nombre: 'Santiago Ariza', puestos: 1}
+    ,'VuZ': {nombre: 'Iris & Salomé Ariza', puestos: 2}
+    ,'0tj': {nombre: 'Edgar & Mery Rueda', puestos: 2}
+    ,'taQ': {nombre: 'Alberto Rueda', puestos: 1}
+    ,'9NB': {nombre: 'Emilio & Gloria Rueda', puestos: 2}
+    ,'aSO': {nombre: 'Gerardo & Alejandra Ríos', puestos: 2}
+    ,'9uM': {nombre: 'Angélica Rueda', puestos: 1}
+    ,'4AX': {nombre: 'Mafe González', puestos: 1}
+    ,'iqk': {nombre: 'Mary & Paula Rueda', puestos: 2}
+    ,'qNx': {nombre: 'Familia López Ariza', puestos: 4}
+    ,'IS8': {nombre: 'Maicol & Laura Villafañe', puestos: 2}
+    ,'2in': {nombre: 'Carmen Escamilla', puestos: 1}
+    ,'9j7': {nombre: 'Familia Ávila', puestos: 3}
+    ,'hXK': {nombre: 'José & Luz marina Corredor', puestos: 2}
+    ,'EU5': {nombre: 'José David & Laura Corredor', puestos: 2}
+    ,'mbM': {nombre: 'Reynel Corredor', puestos: 1}
+    ,'VMd': {nombre: 'Orlando & Yenny Contreras', puestos: 2}
+    ,'IDz': {nombre: 'Daniel Contreras', puestos: 1}
+    ,'jEf': {nombre: 'Juan Diego Contreras', puestos: 1}
+    ,'vVd': {nombre: 'Juan Pablo & Tatiana contreras', puestos: 2}
+    ,'eGt': {nombre: 'Valentina Sánchez', puestos: 1}
+    ,'c29': {nombre: 'Andrés & Marcela Fresneda', puestos: 2}
+    ,'cpQ': {nombre: 'Juan David & Liliana Ruiz', puestos: 2}
+    ,'ZCU': {nombre: 'Sarita Ruiz', puestos: 1}
+    ,'huS': {nombre: 'Wilmar & Andreita Cañón', puestos: 2}
+    ,'hAw': {nombre: 'Diana Moreno', puestos: 1}
+    ,'vTm': {nombre: 'Natalia Moreno', puestos: 1}
+    ,'shY': {nombre: 'Andreina Piela', puestos: 1}
+    ,'wJs': {nombre: 'Sergio Garzón', puestos: 1}
+    ,'iVx': {nombre: 'Nacir Mejía', puestos: 1}
+    ,'rvm': {nombre: 'Orlando UA', puestos: 1}
+    ,'NAa': {nombre: 'Shery Sáenz', puestos: 1}
+    ,'OAQ': {nombre: 'Jhon Castillo', puestos: 1}
+    ,'kI4': {nombre: 'Paula Solano', puestos: 1}
+    ,'3DU': {nombre: 'Aleja Fonseca', puestos: 1}
+};
